ce92cba0d5776db4d5fe42c1ea559768e96eab21
cns2382 <csanford@austin.utexas.edu>
2021-05-22 15:21:24 -0500

    Implement planetService in component with loading logic and basic display

--- src/components/PlanetTable.vue
+++ src/components/PlanetTable.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
     <h1>Planet Info</h1>
+
+    <div v-if="loading">
+      <p>Loading planet data...</p>
+    </div>
+    <div v-else>
+      {{ planets }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'PlanetTable',
+  data() {
+    return {
+      loading: false,
+      planets: []
+    };
+  },
   created() {
-    this.$planetService.getPlanets().then(response => {
-      console.log(response);
-    });
+    this.getPlanets();
+  },
+  methods: {
+    getPlanets() {
+      this.loading = true;
+      this.$planetService.getPlanets().then(response => {
+        this.loading = false;
+        this.planets = response.data.results;
+      });
+    }
   }
 }
 </script>
