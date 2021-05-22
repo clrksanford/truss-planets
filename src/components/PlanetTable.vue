1a33f7d9b14e9f392478d53ffa1f0c79c2e60042
cns2382 <csanford@austin.utexas.edu>
2021-05-22 15:29:24 -0500

    Initial display of planets in table

--- src/components/PlanetTable.vue
+++ src/components/PlanetTable.vue
@@ -6,7 +6,28 @@
       <p>Loading planet data...</p>
     </div>
     <div v-else>
-      {{ planets }}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Climate</th>
+            <th># Residents</th>
+            <th>Terrain</th>
+            <th>Population</th>
+            <th>Surface Area Covered by Water</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="planet of planets" :key="planet.id">
+            <td>{{ planet.name }}</td>
+            <td>{{ planet.climate }}</td>
+            <td>{{ planet.residents.length }}</td>
+            <td>{{ planet.terrain }}</td>
+            <td>{{ planet.population }}</td>
+            <td>?</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
